f3aad3b92e831c731a9245b854a52a3119d991c2
Tom Lindelius <tom.lindelius@gmail.com>
2016-11-23 16:56:43 +0100

    Added convenience methods, plus other fixes, to the BulkBuilder class

--- src/BulkBuilder.php
+++ src/BulkBuilder.php
@@ -10,6 +10,7 @@ use RuntimeException;
  * Class BulkBuilder
  *
  * @author  Tom Lindelius <tom.lindelius@vivamedia.se>
+ * @package Lindelius\LaravelMongo
  * @version 0.3
  */
 class BulkBuilder
@@ -17,6 +18,7 @@ class BulkBuilder
     /**
      * The collection on which to execute the bulk write.
      *
+     * @internal
      * @var Collection|null
      */
     private $collection = null;
@@ -24,6 +26,7 @@ class BulkBuilder
     /**
      * The operations to perform when executing the bulk write.
      *
+     * @internal
      * @var array
      */
     private $operations = [];
@@ -31,6 +34,7 @@ class BulkBuilder
     /**
      * The number of operations that have been added to the bulk.
      *
+     * @internal
      * @var int
      */
     private $operationsCount = 0;
@@ -56,15 +60,20 @@ class BulkBuilder
             $this->collection = $object::collection();
         }
 
-        $this->addRaw($object->asBulkOperation());
+        $bulkOperation = $object->asBulkOperation();
+
+        if ($bulkOperation !== null) {
+            $this->addRaw($bulkOperation);
+        }
     }
 
     /**
-     * Adds a raw operation to the bulk.
+     * Adds a raw bulk operation.
      *
+     * @internal
      * @param array $operation
      */
-    public function addRaw(array $operation)
+    private function addRaw(array $operation)
     {
         $this->operations[] = $operation;
 
@@ -79,6 +88,34 @@ class BulkBuilder
     public function count()
     {
         return $this->operationsCount;
+    }
+
+    /**
+     * Adds a "delete many" bulk operation.
+     *
+     * @param array $filter
+     */
+    public function deleteMany(array $filter)
+    {
+        $this->addRaw([
+            'deleteMany' => [
+                $filter
+            ]
+        ]);
+    }
+
+    /**
+     * Adds a "delete one" bulk operation.
+     *
+     * @param array $filter
+     */
+    public function deleteOne(array $filter)
+    {
+        $this->addRaw([
+            'deleteOne' => [
+                $filter
+            ]
+        ]);
     }
 
     /**
@@ -98,6 +135,38 @@ class BulkBuilder
     }
 
     /**
+     * Adds a "insert one" bulk operation.
+     *
+     * @param array $document
+     */
+    public function insertOne(array $document)
+    {
+        $this->addRaw([
+            'insertOne' => [
+                $document
+            ]
+        ]);
+    }
+
+    /**
+     * Adds a "replace one" bulk operation.
+     *
+     * @param array $filter
+     * @param array $replacement
+     * @param array $options
+     */
+    public function replaceOne(array $filter, array $replacement, array $options = [])
+    {
+        $this->addRaw([
+            'replaceOne' => [
+                $filter,
+                $replacement,
+                $options
+            ]
+        ]);
+    }
+
+    /**
      * Sets the collection on which to execute the bulk write.
      *
      * @param Collection $collection
@@ -105,5 +174,41 @@ class BulkBuilder
     public function setCollection(Collection $collection)
     {
         $this->collection = $collection;
+    }
+
+    /**
+     * Adds a "update many" bulk operation.
+     *
+     * @param array $filter
+     * @param array $update
+     * @param array $options
+     */
+    public function updateMany(array $filter, array $update, array $options = [])
+    {
+        $this->addRaw([
+            'updateMany' => [
+                $filter,
+                $update,
+                $options
+            ]
+        ]);
+    }
+
+    /**
+     * Adds a "update one" bulk operation.
+     *
+     * @param array $filter
+     * @param array $update
+     * @param array $options
+     */
+    public function updateOne(array $filter, array $update, array $options = [])
+    {
+        $this->addRaw([
+            'updateOne' => [
+                $filter,
+                $update,
+                $options
+            ]
+        ]);
     }
 }
